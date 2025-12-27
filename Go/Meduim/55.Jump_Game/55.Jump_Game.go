088d34b2ca69f3737408c5afbd6b0b6bfc307648
Bauka <baykatv5@gmail.com>
2025-12-27 20:27:49 +0500

    solved 55th problem

---
+++ Go/Meduim/55.Jump_Game/55.Jump_Game.go
@@ -0,0 +1,15 @@
+package jumpgame
+
+func canJump(nums []int) bool {
+	maxReach := 0
+
+	for i := 0; i < len(nums); i++ {
+		if i > maxReach {
+			return false
+		}
+		if i+nums[i] > maxReach {
+			maxReach = i + nums[i]
+		}
+	}
+	return true
+}
