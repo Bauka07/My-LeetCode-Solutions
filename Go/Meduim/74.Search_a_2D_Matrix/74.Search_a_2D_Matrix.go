4a95cd806a3ac1a8007299641fa4c9cd06f804bd
Bauka <baykatv5@gmail.com>
2025-12-27 20:36:15 +0500

    solved 74th problem

--- Go/Meduim/74.Search_a_2D_Matrix/74.Search_a_2D_Matrix.go
+++ Go/Meduim/74.Search_a_2D_Matrix/74.Search_a_2D_Matrix.go
@@ -1,1 +1,30 @@
 package searcha2dmatrix
+
+func searchMatrix(matrix [][]int, target int) bool {
+    if len(matrix) == 0 || len(matrix[0]) == 0 {
+        return false
+    }
+
+    m := len(matrix)
+    n := len(matrix[0])
+
+    left, right := 0, m*n-1
+
+    for left <= right {
+        mid := left + (right-left)/2
+
+        r := mid / n
+        c := mid % n
+
+        val := matrix[r][c]
+
+        if val == target {
+            return true
+        } else if val < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return false
+}
